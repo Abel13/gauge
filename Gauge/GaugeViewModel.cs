63116b234282ff488dbf8cda02e0b71ebbcf7db0
abel <abel.o.d@outlook.com>
2019-08-03 16:09:35 -0300

    first commit

---
+++ Gauge/GaugeViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauge
+{
+    public class GaugeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if(PropertyChanged!= null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public GaugeViewModel()
+        {
+            Angle = -85;
+            Value = 0;
+        }
+
+        int _angle;
+        public int Angle
+        {
+            get
+            {
+                return _angle;
+            }
+
+            private set
+            {
+                _angle = value;
+                NotifyPropertyChanged("Angle");
+            }
+        }
+
+        int _value;
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                if (value >= 0 && value <= 170)
+                {
+                    _value = value;
+                    Angle = value - 85;
+                    NotifyPropertyChanged("Value");
+                }
+            }
+        }
+    }
+}
---
+++ Gauge/UserControlGauge.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gauge
+{
+    /// <summary>
+    /// Interaction logic for UserControlGauge.xaml
+    /// </summary>
+    public partial class UserControlGauge : UserControl
+    {
+        public UserControlGauge()
+        {
+            InitializeComponent();
+        }
+    }
+}
